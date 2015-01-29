252b614136502af373b16dd2d028dd460a254147
TOSHIBAZ830 <TOSHIBAZ830@TOSHIBAZ830-PC.mshome.net>
2015-01-29 12:51:14 +0900

    mybatis 연동 설정

--- web_test_03/src/main/java/net/hwanee/www/HomeController.java
+++ web_test_03/src/main/java/net/hwanee/www/HomeController.java
@@ -3,6 +3,11 @@ package net.hwanee.www;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import net.hwanee.www.services.TestServices;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Handles requests for the application home page.
@@ -22,8 +28,13 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+	
+	@Inject
+	private TestServices testServices;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(@RequestParam Map<String, String> paramMap,
+			Locale locale, Model model) throws Exception {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
@@ -32,6 +43,12 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
+		paramMap.put("pageNo", "1");
+		paramMap.put("pageRow", "10");
+		paramMap.put("StartPage", String.valueOf((Integer.parseInt(paramMap.get("pageNo"))-1)*Integer.parseInt(paramMap.get("pageRow"))));
+		
+		model.addAttribute("testdb", testServices.getList(paramMap));
 		
 		return "home";
 	}
